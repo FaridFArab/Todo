2b6efc815321a8d4d5bc25a03b7822b5a49a1238
PsychoFarid <faridarab4218@gmail.com>
2021-07-01 00:01:16 +0430

    Add login endpoint and token.

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('todo/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
     path('todo/<int:pk>/complete', views.TodoComplete.as_view()),
     path('todo/completed/', views.TodoCompletedList.as_view()),
+    path('signup', views.signup),
 ]
